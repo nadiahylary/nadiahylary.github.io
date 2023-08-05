7ebfe46f49677d9a02057cb99a3d6fed801a832c
Nadia HYLARY <nhylary12@gmail.com>
2023-08-06 00:08:28 +0100

    added completed projects media

--- assets/index.js
+++ assets/index.js
@@ -6,3 +6,20 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+window.onload = function () {
+    setInterval(loadIntro, 2000)
+}
+
+var intros = ["Mobile Developer", "Feminist", "Backend Developer", "Content Creator", "Yogi", "Lifelong Learner"]
+var index = 0;
+var intro = document.getElementById("intro-loader")
+
+function loadIntro() {
+    intro.innerHTML = intros[index];
+    intro.classList.add("fade");
+    index++;
+    if (index >= intros.length) {
+        index = 0;
+    }
+}
